46ffe1272b59da435af86e852d467bdbcb1d329f
Pablo Messa <pablomessa@gmail.com>
2023-09-27 20:38:24 -0300

    hook useItemCollection

--- src/pages/ItemsDetails.jsx
+++ src/pages/ItemsDetails.jsx
@@ -1,36 +1,19 @@
-import { useEffect, useState } from 'react';
 
-import { useParams } from 'react-router-dom'
+import { useItemCollection } from '../hooks/useItemCollection';
 
+import { useParams } from 'react-router-dom';
 import ItemDetailContainer from '../components/ItemDetailContainer/ItemDetailContainer'
-
-import { doc, getDoc, getFirestore } from "firebase/firestore";
+import LoaderComponent from '../components/LoaderComponent/LoaderComponent';
 
 const ItemsDetails = () => {
 
- 
 
-  const [products, setProducts] = useState([]);
+  const {itemId} = useParams();
 
-  const { itemId } = useParams();
-
-  useEffect(() => {
-    const db = getFirestore();
-    const item = doc(db, "products", itemId);
-
-    getDoc(item)
-      .then((snapshot) => {
-        setProducts({ id: snapshot.id, ...snapshot.data() });
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-    
-  }, [itemId])
-
+  const { data, loadin } = useItemCollection("products", itemId)
   return (
 
-    <ItemDetailContainer DetalleProducto={products} />
+    loadin ? <LoaderComponent /> : <ItemDetailContainer DetalleProducto={data} />
 
   );
 };
