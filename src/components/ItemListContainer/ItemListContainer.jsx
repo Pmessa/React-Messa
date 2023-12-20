7dd7a060aef2011c3e0a8fe93ab4d69c1be324a5
Your Name <pablomessa@gmail.com>
2023-12-20 11:57:52 -0300

    nuevo commit

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -12,18 +12,18 @@ const ItemListContainer = ({ ProductsData }) => {
         return (
           <div className='listItemsContainer' key={item.id}>
             <div className='imgHomeContainer'>
-              <img className='imgHome' src={item.img} alt="Imágen del producto" onClick={() => Navigate(`/img/${item.id}`)}/>
+              <img className='imgHome' src={item.img} alt="Imágen del producto"/>
             </div>
             <div className='descriptionContainer'>
               <h1 className='titleContainer'>{item.title}</h1>
               <h3 className='description'>{item.description}</h3>
+            </div>
               <div className='buttonContainer'>
                 <button onClick={() => Navigate(`/item/${item.id}`)} className='buttonVerDetalle'>Ver Detalle</button>
               </div>
-            </div>
           </div>
-                  );
-      })};
+                  )
+      })}
     </div>
   );
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,9 @@
 import CartWidget from '../CartWidget/CartWidget';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
+// import Container from 'react-bootstrap/Container';
+// import Form from 'react-bootstrap/Form';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
 
 import "./Navbar.css"
 
@@ -11,36 +11,51 @@ import { Link } from 'react-router-dom';
 
 const NavBar = () =>{
   return (
-    <Navbar expand="lg" className="bg-body">
-      <Container fluid className='container'>
-        <Navbar.Brand><Link to="/"><img src="/logo-tr.png" alt="logo de la marca" className='imgLogo'/></Link></Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-            >
-            <Nav.Link><Link  to="/">Alimentos Saludables</Link></Nav.Link>
-            <NavDropdown title="Productos" id="navbarScrollingDropdown">
-              <NavDropdown.Item>
-              <Link to={"/category/CGluten"} className='drop'>Con Gluten</Link>
-              </NavDropdown.Item>
-              <NavDropdown.Item>
-              <NavDropdown.Divider/>
-                <Link to={"/category/STACC"} className='drop'>Sin TACC</Link>
-              </NavDropdown.Item>
+    <nav className="nav">
+      <div className="logoContainer">
+      <Link to="/"><img src="/logo-tr.png" alt="logo de la marca" className='imgLogo'/></Link>
+      <CartWidget/>
+      </div>
+      <ul className="buttonList">
+        <li><Link to={"/category/CGluten"} className='drop'><button className="btn Con Gluten">Con Gluten</button></Link></li>
+        <li>  <Link to={"/category/STACC"} className='drop'><button className="btn Sin TACC">Sin TACC</button></Link></li>
+        <li><Link  to="/"><button className="btn Opciones">Nuestras Opciones</button></Link></li>
+      </ul>
+    </nav>
 
-
-            </NavDropdown>
-          </Nav>
-
-        </Navbar.Collapse>
-          <CartWidget/>
-      </Container>
-
-      </Navbar>
   );
 }
 
 export default NavBar;
+
+
+
+{/* <Navbar expand="lg" className="bg-body">
+<Container fluid className='container'>
+  <Navbar.Brand><Link to="/"><img src="/logo-tr.png" alt="logo de la marca" className='imgLogo'/></Link></Navbar.Brand>
+  <Navbar.Toggle aria-controls="navbarScroll" />
+  <Navbar.Collapse id="navbarScroll">
+    <Nav
+      className="me-auto my-2 my-lg-0"
+      style={{ maxHeight: '100px' }}
+      navbarScroll
+      >
+      <Nav.Link><Link  to="/">Alimentos Saludables</Link></Nav.Link>
+      <NavDropdown title="Productos" id="navbarScrollingDropdown">
+        <NavDropdown.Item>
+        <Link to={"/category/CGluten"} className='drop'>Con Gluten</Link>
+        </NavDropdown.Item>
+        <NavDropdown.Item>
+        <NavDropdown.Divider/>
+          <Link to={"/category/STACC"} className='drop'>Sin TACC</Link>
+        </NavDropdown.Item>
+
+
+      </NavDropdown>
+    </Nav>
+
+  </Navbar.Collapse>
+    <CartWidget/>
+</Container>
+
+</Navbar> */}
