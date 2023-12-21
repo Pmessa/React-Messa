f8da5322bf39434e89ac8a6b0345c696e81f1309
Your Name <pablomessa@gmail.com>
2023-12-21 08:45:54 -0300

    cart commit

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -2,29 +2,26 @@ import { useContext } from "react";
 import "./CartWidget.css";
 import 'animate.css';
 import { CartContext } from "../../context/CartContext";
-import { Button } from "react-bootstrap";
 import { Link } from 'react-router-dom';
 
-const CartWidget =() =>{
+const CartWidget = () => {
 
-  const {cartItems} = useContext(CartContext)
+  const { cartItems } = useContext(CartContext)
 
 
-    return (
-      <div className="cart-widget">
-        <Link  to="/item/itemId">
-        <Button variant="success"  className='buttonVerCart'>
-<div className='CartButtonContainer'>
-<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" className="bi bi-cart4" viewBox="0 0 16 16">
-  <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
-</svg>
-</div>
-</Button>
+  return (
+    <div className="cart-widget">
+
+      <div className="cartWidgetLink">
+        <Link to="/item/itemId"><img src="../../../public/cart-tr.png" alt="cartImg" />
         </Link>
+        
+        <strong className="animate__animated animate__bounce animate__delay-5s animate__slower animate__infinite cantCarrito cartNumber">{cartItems}</strong>
+      </div>
 
-<strong className="animate__animated animate__bounce animate__delay-2s animate__slower animate__infinite cantCarrito">{ cartItems }</strong>
-</div>
+    </div>
 
-    )
+
+  )
 }
 export default CartWidget;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,5 @@
 import CartWidget from '../CartWidget/CartWidget';
-// import NavDropdown from 'react-bootstrap/NavDropdown';
-// import Container from 'react-bootstrap/Container';
-// import Form from 'react-bootstrap/Form';
-// import Nav from 'react-bootstrap/Nav';
-// import Navbar from 'react-bootstrap/Navbar';
+
 
 import "./Navbar.css"
 
@@ -17,9 +13,9 @@ const NavBar = () =>{
       <CartWidget/>
       </div>
       <ul className="buttonList">
-        <li><Link to={"/category/CGluten"} className='drop'><button className="btn Con Gluten">Con Gluten</button></Link></li>
-        <li>  <Link to={"/category/STACC"} className='drop'><button className="btn Sin TACC">Sin TACC</button></Link></li>
-        <li><Link  to="/"><button className="btn Opciones">Nuestras Opciones</button></Link></li>
+        <li><Link to={"/category/CGluten"} className='drop btn ConGluten'>Con Gluten</Link></li>
+        <li><Link to={"/category/STACC"} className='drop btn SinTACC'>Sin TACC</Link></li>
+        <li><Link  to="/" className="btn Opciones">Nuestras Opciones</Link></li>
       </ul>
     </nav>
 
@@ -29,33 +25,3 @@ const NavBar = () =>{
 export default NavBar;
 
 
-
-{/* <Navbar expand="lg" className="bg-body">
-<Container fluid className='container'>
-  <Navbar.Brand><Link to="/"><img src="/logo-tr.png" alt="logo de la marca" className='imgLogo'/></Link></Navbar.Brand>
-  <Navbar.Toggle aria-controls="navbarScroll" />
-  <Navbar.Collapse id="navbarScroll">
-    <Nav
-      className="me-auto my-2 my-lg-0"
-      style={{ maxHeight: '100px' }}
-      navbarScroll
-      >
-      <Nav.Link><Link  to="/">Alimentos Saludables</Link></Nav.Link>
-      <NavDropdown title="Productos" id="navbarScrollingDropdown">
-        <NavDropdown.Item>
-        <Link to={"/category/CGluten"} className='drop'>Con Gluten</Link>
-        </NavDropdown.Item>
-        <NavDropdown.Item>
-        <NavDropdown.Divider/>
-          <Link to={"/category/STACC"} className='drop'>Sin TACC</Link>
-        </NavDropdown.Item>
-
-
-      </NavDropdown>
-    </Nav>
-
-  </Navbar.Collapse>
-    <CartWidget/>
-</Container>
-
-</Navbar> */}
