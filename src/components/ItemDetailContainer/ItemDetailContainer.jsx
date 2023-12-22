1a6f7e925d8f41eacad6b6ecef44a63afe3b9d48
pmessa <pablomessa@gmail.com>
2023-12-21 23:22:56 -0300

    cambios en home css cards

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -38,7 +38,7 @@ const ItemDetailContainer = ({ DetalleProducto }) => {
       </section>
       
         <div className="detalleInfoProducto">
-           <h3 className="detalleInfoTitle">Porqué aprobechar {DetalleProducto.title}</h3>
+           <h3 className="detalleInfoTitle">Porqué aprovechar {DetalleProducto.title}</h3>
            <p>{DetalleProducto.detail}</p>
         </div>
     </div>
@@ -46,20 +46,3 @@ const ItemDetailContainer = ({ DetalleProducto }) => {
 }
 export default ItemDetailContainer
 
-
-{/* <h1>{DetalleProducto.title}</h1>
-    
-<div className='CardContainer'>
-  <div className='ImgContainer'>
-  <img src={DetalleProducto.img} alt="Imagen del producto" />
-  </div>
-  <ListGroup>
-  <ListGroup.Item><h2>{DetalleProducto.description}</h2></ListGroup.Item>
-  <ListGroup.Item>{DetalleProducto.detail}</ListGroup.Item>
-  <ListGroup.Item><p className='$'><span>$</span></p>{DetalleProducto.price}</ListGroup.Item>
-  <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-</ListGroup>
-  </div>
-  <ItemCount />
-  { stock > 5 ? <strong>Producto Disponible</strong> : <strong>Ultimas unidades!</strong>}
-  <Button className="btn-success" onClick={()=> setStock(stock - 1)}>Agregar al Carrito</Button> */}
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -10,7 +10,7 @@ const Cart= () => {
 
     loadin ? <LoaderComponent /> : <CartComponent CartProduct={data} />
 
-  );
+  )
 };
 
-export default Cart
+export default Cart;
